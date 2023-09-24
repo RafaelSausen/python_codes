3b4887753eb4ab28073df898d3fc49f790e7e653
Rafael Sausen Bahr <rafaelsausen@gmail.com>
2023-09-24 19:30:50 -0300

    FIX: Sort with repeated elements

--- ordination/bubble_sort.py
+++ ordination/bubble_sort.py
@@ -5,7 +5,7 @@ class BubbleSort:
             swapped = False
             for firstIndex in range(0, len(numbers) - 1):
                 secondIndex = firstIndex + 1
-                if numbers[firstIndex] >= numbers[secondIndex]:
+                if numbers[firstIndex] > numbers[secondIndex]:
                     swapping = numbers[firstIndex]
                     numbers[firstIndex] = numbers[secondIndex]
                     numbers[secondIndex] = swapping
--- ordination/bubble_sort_test.py
+++ ordination/bubble_sort_test.py
@@ -11,5 +11,11 @@ class BubbleSortTest(unittest.TestCase):
         resultArray = self.bubbleSort.execute(aleatoryNumbers)
         self.assertListEqual(orderedNumbers, resultArray, "Should return the ordered numbers")
 
+    def test_execute_with_repeated_elements(self) -> None:
+        aleatoryNumbers = [5, 18, 10, 49, 16, 49, 10, 82, 43, 12 ]
+        orderedNumbers = [ 5, 10, 10, 12, 16, 18, 43, 49, 49, 82 ]
+        resultArray = self.bubbleSort.execute(aleatoryNumbers)
+        self.assertListEqual(orderedNumbers, resultArray, "Should return the ordered numbers")
+
 if __name__ == "__main__":
     unittest.main()
